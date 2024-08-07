76b86fc11dec7bee13da6fd3e85e485c51652da1
HJLee <>
2024-08-07 18:35:11 +0900

    Player, Monster

--- Assets/@Scripts/Managers/Managers.cs
+++ Assets/@Scripts/Managers/Managers.cs
@@ -7,6 +7,14 @@ public class Managers : MonoBehaviour
     private static Managers s_instance;
     private static Managers Instance { get { Init(); return s_instance; } }
 
+    #region Contents
+    private GameManager   _game   = new GameManager();
+    private ObjectManager _object = new ObjectManager();
+
+    public static GameManager   Game   { get { return Instance?._game;   } }
+    public static ObjectManager Object { get { return Instance?._object; } }
+    #endregion
+
     #region CORE
     private DataManager     _data     = new DataManager();
     private PoolManager     _pool     = new PoolManager();
@@ -15,12 +23,12 @@ public class Managers : MonoBehaviour
     private SoundManager    _sound    = new SoundManager();
     private UIManager       _ui       = new UIManager();
 
-    public static DataManager     Data     { get { return Instance?._data; } }
-    public static PoolManager     Pool     { get { return Instance?._pool; } }
+    public static DataManager     Data     { get { return Instance?._data;     } }
+    public static PoolManager     Pool     { get { return Instance?._pool;     } }
     public static ResourceManager Resource { get { return Instance?._resource; } }
-    public static SceneManagerEx  Scene    { get { return Instance?._scene; } }
-    public static SoundManager    Sound    { get { return Instance?._sound; } }
-    public static UIManager       UI       { get { return Instance?._ui; } }
+    public static SceneManagerEx  Scene    { get { return Instance?._scene;    } }
+    public static SoundManager    Sound    { get { return Instance?._sound;    } }
+    public static UIManager       UI       { get { return Instance?._ui;       } }
     #endregion
 
     public static void Init()
--- Assets/@Scripts/Scenes/GameScene.cs
+++ Assets/@Scripts/Scenes/GameScene.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static Define;
 
 public class GameScene : BaseScene
 {
@@ -13,6 +15,22 @@ public class GameScene : BaseScene
 
         SceneType = Define.EScene.GameScene;
 
+        GameObject map = Managers.Resource.Instantiate("BaseMap");
+        map.transform.position = Vector3.zero;
+        map.name = "@BaseMap";
+
+        Hero hero = Managers.Object.Spawn<Hero>(Vector3.zero);
+        hero.CreatureState = Define.ECreatureState.Move;
+
+        Managers.UI.ShowBaseUI<UI_Joystick>();
+
+        CameraController camera = Camera.main.GetOrAddComponent<CameraController>();
+        camera.Target = hero;
+
+        {
+            Monster monster = Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0));
+            monster.CreatureState = ECreatureState.Idle;
+        }
         // TODO
 
         return true;
